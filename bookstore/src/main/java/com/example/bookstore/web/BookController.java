0b8c2580f614ce11f45c2530444d52a3c58bf715
DooMi- <hurmerintaeemeli@gmail.com>
2025-01-27 13:40:59 +0200

    Chapter 3. Task 3 finished. Added crud functionality: can add and delete books.

--- bookstore/src/main/java/com/example/bookstore/web/BookController.java
+++ bookstore/src/main/java/com/example/bookstore/web/BookController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BookController {
@@ -23,4 +26,23 @@ public class BookController {
         model.addAttribute("books", bookRepository.findAll());
         return "booklist"; // Thymeleaf template name
     }
+
+    @GetMapping("/add")
+    public String addBookForm(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @PostMapping("/save")
+    public String saveBook(@ModelAttribute Book book) {
+        bookRepository.save(book);
+        return "redirect:/booklist";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long id) {
+        bookRepository.deleteById(id);
+        return "redirect:/booklist";
+    }
+
 }
