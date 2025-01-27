2f06341bf0337b7bf86d242628e011754374b194
DooMi- <hurmerintaeemeli@gmail.com>
2025-01-27 13:25:47 +0200

    Chapter 3. Task 2 finished. Added Bookstore: List page

--- bookstore/src/main/java/com/example/bookstore/web/BookController.java
+++ bookstore/src/main/java/com/example/bookstore/web/BookController.java
@@ -1,5 +1,8 @@
 package com.example.bookstore.web;
 
+import com.example.bookstore.domain.Book;
+import com.example.bookstore.domain.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +10,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BookController {
 
-    @GetMapping("/index")
-    public String showIndex(Model model) {
-        model.addAttribute("message", "Welcome to the Bookstore!");
-        return "index";
+    private final BookRepository bookRepository;
+
+    // Inject BookRepository via constructor
+    @Autowired
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @GetMapping("/booklist")
+    public String listBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "booklist"; // Thymeleaf template name
     }
 }
